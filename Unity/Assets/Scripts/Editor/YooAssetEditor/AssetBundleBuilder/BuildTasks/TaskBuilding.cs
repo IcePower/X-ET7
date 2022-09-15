f30a1d1202bfb2c14f82bde2f1e172737fe9b6e6
IcePower <IcePower@git.com>
2022-09-15 10:43:23 +0800

    更新 YooAsset af29cb7

--- Unity/Assets/Scripts/Editor/YooAssetEditor/AssetBundleBuilder/BuildTasks/TaskBuilding.cs
+++ Unity/Assets/Scripts/Editor/YooAssetEditor/AssetBundleBuilder/BuildTasks/TaskBuilding.cs
@@ -29,6 +29,13 @@ namespace YooAsset.Editor
 			AssetBundleManifest buildResults = BuildPipeline.BuildAssetBundles(buildParametersContext.PipelineOutputDirectory, buildMapContext.GetPipelineBuilds(), opt, buildParametersContext.Parameters.BuildTarget);
 			if (buildResults == null)
 				throw new Exception("构建过程中发生错误！");
+			
+			if (buildMode == EBuildMode.ForceRebuild || buildMode == EBuildMode.IncrementalBuild)
+			{
+				string unityOutputManifestFilePath = $"{buildParametersContext.PipelineOutputDirectory}/{YooAssetSettings.OutputFolderName}";
+				if(System.IO.File.Exists(unityOutputManifestFilePath) == false)
+					throw new Exception("构建过程中发生严重错误！请查阅上下文日志！");
+			}
 
 			BuildRunner.Log("Unity引擎打包成功！");
 			BuildResultContext buildResultContext = new BuildResultContext();
