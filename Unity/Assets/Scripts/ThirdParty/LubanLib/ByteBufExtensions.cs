1437fbea7c84e891efba45723a37e048487b8290
LiBing <libing@ofunagames.com>
2022-09-16 17:57:19 +0800

    服务端的 UnityEngin 相关代码被熊猫删了，服务端无法使用了。

--- Unity/Assets/Scripts/ThirdParty/LubanLib/ByteBufExtensions.cs
+++ Unity/Assets/Scripts/ThirdParty/LubanLib/ByteBufExtensions.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
+#if UNITY
 namespace Bright.Serialization
 {
     public static class ByteBufExtensions
@@ -45,3 +41,4 @@ namespace Bright.Serialization
         }
     }
 }
+#endif
