76273b777fc95baa003f6e474f4fb5d8d30485e4
IcePower <icepower@git.com>
2024-03-01 18:38:05 +0800

    同步ET的EntityRef

--- Unity/Assets/Scripts/Core/Module/Entity/EntityRef.cs
+++ Unity/Assets/Scripts/Core/Module/Entity/EntityRef.cs
@@ -2,13 +2,19 @@ using System;
 
 namespace ET
 {
-    public readonly struct EntityRef<T> where T: Entity
+    public struct EntityRef<T> where T: Entity
     {
         private readonly long instanceId;
-        private readonly T entity;
+        private T entity;
 
         private EntityRef(T t)
         {
+            if (t == null)
+            {
+                this.instanceId = 0;
+                this.entity = null;
+                return;
+            }
             this.instanceId = t.InstanceId;
             this.entity = t;
         }
@@ -23,7 +29,8 @@ namespace ET
                 }
                 if (this.entity.InstanceId != this.instanceId)
                 {
-                    return null;
+                    // 这里instanceId变化了，设置为null，解除引用，好让runtime去gc
+                    this.entity = null;
                 }
                 return this.entity;
             }
@@ -35,6 +42,61 @@ namespace ET
         }
 
         public static implicit operator T(EntityRef<T> v)
+        {
+            return v.UnWrap;
+        }
+    }
+    
+    
+    public struct EntityWeakRef<T> where T: Entity
+    {
+        private long instanceId;
+        // 使用WeakReference，这样不会导致entity dispose了却无法gc的问题
+        // 不过暂时没有测试WeakReference的性能
+        private readonly WeakReference<T> weakRef;
+
+        private EntityWeakRef(T t)
+        {
+            if (t == null)
+            {
+                this.instanceId = 0;
+                this.weakRef = null;
+                return;
+            }
+            this.instanceId = t.InstanceId;
+            this.weakRef = new WeakReference<T>(t);
+        }
+        
+        private T UnWrap
+        {
+            get
+            {
+                if (this.instanceId == 0)
+                {
+                    return null;
+                }
+
+                if (!this.weakRef.TryGetTarget(out T entity))
+                {
+                    this.instanceId = 0;
+                    return null;
+                }
+
+                if (entity.InstanceId != this.instanceId)
+                {
+                    this.instanceId = 0;
+                    return null;
+                }
+                return entity;
+            }
+        }
+        
+        public static implicit operator EntityWeakRef<T>(T t)
+        {
+            return new EntityWeakRef<T>(t);
+        }
+
+        public static implicit operator T(EntityWeakRef<T> v)
         {
             return v.UnWrap;
         }
